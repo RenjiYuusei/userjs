c5f54305591c38dff44dd316376ea9308df1db23
Renji Yuusei <166010224+RenjiYuusei@users.noreply.github.com>
2025-11-09 19:08:08 +0700

    Add user script for UGPhone login

---
+++ ugphone.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Kasumi UGPhone Login
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  Đăng nhập UGPhone bằng LocalStorage - Made by RenjiYuusei
+// @author       RenjiYuusei
+// @match        https://www.ugphone.com/*
+// @icon         https://cdn.discordapp.com/icons/1417948424346800253/6623d988a9ffb81bb7abdba67a40a391.png?size=4096
+// @require      https://pastefy.app/aDlCJKdB/raw
+// @grant        none
+// ==/UserScript==
