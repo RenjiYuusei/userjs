95283806bb0ce6ce7336508e17b305749519fc55
Renji Yuusei <166010224+RenjiYuusei@users.noreply.github.com>
2025-11-13 10:53:46 +0700

    Update match pattern and add download/update URLs

--- ugphone.user.js
+++ ugphone.user.js
@@ -4,8 +4,10 @@
 // @version      1.2
 // @description  Đăng nhập UGPhone bằng LocalStorage - Made by RenjiYuusei
 // @author       RenjiYuusei
-// @match        https://www.ugphone.com/*
+// @match        https://www.ugphone.com/toc-portal/*/login
 // @icon         https://cdn.discordapp.com/icons/1417948424346800253/6623d988a9ffb81bb7abdba67a40a391.png?size=4096
 // @require      https://pastefy.app/aDlCJKdB/raw
+// @downloadURL  https://raw.githubusercontent.com/RenjiYuusei/userjs/main/ugphone.user.js
+// @updateURL    https://raw.githubusercontent.com/RenjiYuusei/userjs/main/ugphone.user.js
 // @grant        none
 // ==/UserScript==
