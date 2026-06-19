d4b28194163da8e6d334ae126873bb2c346afbce
Renji Yuusei <166010224+RenjiYuusei@users.noreply.github.com>
2026-06-19 23:15:45 +0700

    Update version and match URL in user script

--- ugphone.user.js
+++ ugphone.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Kasumi UGPhone Login
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Đăng nhập UGPhone bằng LocalStorage - Made by RenjiYuusei
 // @author       RenjiYuusei
-// @match        https://www.ugphone.com/toc-portal/*/login
+// @match        https://www.ugphone.com/toc-portal/*
 // @icon         https://cdn.discordapp.com/icons/1417948424346800253/6623d988a9ffb81bb7abdba67a40a391.png?size=4096
 // @require      https://pastefy.app/aDlCJKdB/raw
 // @downloadURL  https://raw.githubusercontent.com/RenjiYuusei/userjs/main/ugphone.user.js
