91531af9f5c9a40a20ab80ded70b89abe45d2036
Renji Yuusei <166010224+RenjiYuusei@users.noreply.github.com>
2026-07-09 11:04:54 +0700

    Update ugphone.user.js

--- ugphone.user.js
+++ ugphone.user.js
@@ -1,2 +1,13 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+// ==UserScript==
+// @name         Kasumi UGPhone Login
+// @namespace    http://tampermonkey.net/
+// @version      1.3
+// @description  Đăng nhập UGPhone bằng LocalStorage - Made by RenjiYuusei
+// @author       RenjiYuusei
+// @match        https://www.ugphone.com/toc-portal/*
+// @icon         https://iili.io/Cxt9XZF.jpg
+// @require      https://pastefy.app/aDlCJKdB/raw
+// @downloadURL  https://raw.githubusercontent.com/RenjiYuusei/userjs/main/ugphone.user.js
+// @updateURL    https://raw.githubusercontent.com/RenjiYuusei/userjs/main/ugphone.user.js
+// @grant        none
+// ==/UserScript==
