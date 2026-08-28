7250ec7c2afefa59d9331f8743a36e325bc87aff
Renji Yuusei <166010224+RenjiYuusei@users.noreply.github.com>
2026-08-28 09:04:51 +0700

    Update ugphone.user.js

--- ugphone.user.js
+++ ugphone.user.js
@@ -5,7 +5,7 @@
 // @description  Đăng nhập UGPhone bằng LocalStorage - Made by RenjiYuusei
 // @author       RenjiYuusei
 // @match        https://www.ugphone.com/toc-portal/*
-// @require      https://pastefy.app/aDlCJKdB/raw
+// @require      https://pastefy.app/ESpGwbon/raw
 // @downloadURL  https://raw.githubusercontent.com/RenjiYuusei/userjs/main/ugphone.user.js
 // @updateURL    https://raw.githubusercontent.com/RenjiYuusei/userjs/main/ugphone.user.js
 // @grant        none
